24145134afd1f8a5752f58d1986fb41676c8076b
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-10-09 19:47:48 +0200

    Additional-settings-for-adding-photos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { startServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConection.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
   startServer();
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
 };
 
 bootstrap();
