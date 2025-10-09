441dfed7eb5b3b5d80701c02d0cc3bf2ce68094d
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-10-09 19:40:28 +0200

    Password-reset-add

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,15 @@
 import path from 'node:path';
+import handlebars from 'handlebars';
+import fs from 'node:fs/promises';
+import { UsersCollection } from '../db/models/user';
+import createHttpError from 'http-errors';
+import { getEnvVar } from '../utils/getEnvVar';
+import { sendEmail } from '../utils/sendMail';
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+const jwt = require('jsonwebtoken');
 
 export const SORT_ORDER = {
   ASC: 'asc',
@@ -32,3 +40,42 @@ export const ROLES = {
   PARENT: 'parent',
 };
 
+
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    getEnvVar('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
+  );
+
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.name,
+    link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+  });
+
+  await sendEmail({
+    from: getEnvVar(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html,
+  });
+};
